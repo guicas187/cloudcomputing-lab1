72cdf369081489cb80c27166dab84ebede73c281
Luis Aguirre <luis.aguirre@washpost.com>
2026-02-01 21:36:55 -0600

    New version

--- main.cpp
+++ main.cpp
@@ -1,26 +1,51 @@
 #include <iostream>
 #include <omp.h>
 
+#define N 1000
+#define chunk 100
+#define mostrar 10
+
 using namespace std;
 
+void imprimeArreglo(float *d);
+
 int main() {
-    const int N = 10;
 
-    int A[N], B[N], R[N];
+    cout << "Sumando Arreglos en Paralelo!\n";
 
-    for (int i = 0; i < N; i++) {
-        A[i] = i + 1;
-        B[i] = i + 11;
+    float a[N], b[N], c[N];
+    int i;
+
+    // Inicializar arreglos
+    for (i = 0; i < N; i++) {
+        a[i] = i * 10;
+        b[i] = (i + 3) * 3.7;
     }
 
-    #pragma omp parallel for
-    for (int i = 0; i < N; i++) {
-        R[i] = A[i] + B[i];
+    int pedazos = chunk;
+
+    // Suma paralela
+    #pragma omp parallel for shared(a,b,c,pedazos) private(i) schedule(static,pedazos)
+    for (i = 0; i < N; i++) {
+        c[i] = a[i] + b[i];
     }
 
-    for (int i = 0; i < N; i++) {
-        cout << R[i] << " ";
-    }
+    // Imprimir resultados
+    cout << "Primeros " << mostrar << " valores de A:\n";
+    imprimeArreglo(a);
+
+    cout << "Primeros " << mostrar << " valores de B:\n";
+    imprimeArreglo(b);
+
+    cout << "Primeros " << mostrar << " valores de C:\n";
+    imprimeArreglo(c);
 
     return 0;
+}
+
+void imprimeArreglo(float *d) {
+    for (int x = 0; x < mostrar; x++) {
+        cout << d[x] << " ";
+    }
+    cout << endl;
 }
